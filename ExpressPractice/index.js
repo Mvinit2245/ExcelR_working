684cfacd6aa65bc26410a95f64bfd58549582b9a
Mvinit2245 <vinitmorankar22@gmail.com>
2026-07-17 19:35:20 +0530

    Javascript commit

--- ExpressPractice/index.js
+++ ExpressPractice/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
 //const port = 3000
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send({status:1,msg:"Hello Taloda"})
@@ -9,6 +10,12 @@ app.get('/', (req, res) => {
 // app.listen(port, () => { 
 //   console.log(`Example app listening on port ${port}`)
 // })
+
+app.post('/login', (req,res)=>{
+    console.log(req.body)//object
+    
+    res.send({status:1,msg:"Login API",data:req.body})
+})
 console.log("start the server on port no 8000")
 
 app.listen("8000")
